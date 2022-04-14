4b31183e388c21b1aa874723c66be1f236df8c8c
Clayton <cdston@gmail.com>
2022-04-14 20:03:39 -0300

    Aprendendo sobre seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /* Criar os tipos seeder
+            rodar php artisan db:seed para rodar todas as seeds
+         */
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            TipoSeeder::class,
+            FinalidadeSeeder::class,
+            ProximidadeSeeder::class
+        ]);
     }
 }
